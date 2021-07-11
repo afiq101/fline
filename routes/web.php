8e2a8dcb126e897e829a88354c48460907c4b00c
Adi Iman <adiimn@hotmail.com>
2021-07-11 18:06:36 +0800

    user like controller, axios

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserLikeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,5 @@ Route::get('/', 'HomeController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('like', UserLikeController::class);
