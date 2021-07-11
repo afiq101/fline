a3fcadbc7a13f0d82b489d75c7dae2ffcaa6a57a
ijmmni99 <nasharul99@gmail.com>
2021-07-11 23:22:26 +0800

    update profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -129,21 +129,29 @@
             text-decoration: none;
         }
 
+        .dark-bg{
+            max-width: 488px; width: 100%;background: linear-gradient(0deg, rgba(31, 26, 29, 0.3), rgba(31, 26, 29, 0.3))
+        }
+
     </style>
 @endsection
 
 @section('content')
-    <div class="container-fluid rounded p-3" style="background-color: #1f1f1f">
-        <div class="container" style="max-width: 488px; width: 100%;">
+    <div class="container-fluid rounded p-3" @isset(auth()->user()->userimage) style="background-image: url({{ asset('assets/images/media/img1.jpg')}})" @else style="background-image: url({{ asset('assets/images/media/img2.jpg')}})" @endisset>
+        <div class="container dark-bg">
             <div class="d-flex flex-column">
                 <div class="contain hovereffect d-flex justify-content-center p-3">
+                    @isset(auth()->user()->userimage)
                     <img style="width:200px; height:200px; border-radius:50%" src="{{ asset('assets/images/profile/'.auth()->user()->userimage) }}" alt="">
+                    @else
+                    <img style="width:200px; height:200px; border-radius:50%" src="{{ asset('assets/images/profile/default-photo.png')}}" alt="">
+                    @endisset
                 </div>
                 <div class="p-3 text-center row justify-content-center">
                     <div class="col-12">
                         <h3>{{auth()->user()->name}}</h3>
                     </div>
-                    <div class="col-12 text-center row p-3 justify-content-center" style="color: #636b6f">
+                    <div class="col-12 text-center row p-3 justify-content-center">
                         <div class="col-4 row" >
                             <div class="col-12">
                                 <h4>Like</h4>
@@ -331,6 +339,86 @@
         </div>
     </div>
 
+    <div class="modal fade" id="editprofileModal" tabindex="-1" role="dialog" aria-labelledby="editprofileModal" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-body p-0">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card">
+                                <div class="card-header">{{ __('Register') }}</div>
+                
+                                <div class="card-body">
+                                    <form method="POST" action="{{ route('register') }}">
+                                        @csrf
+                
+                                        <div class="form-group row">
+                                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                
+                                            <div class="col-md-6">
+                                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                
+                                                @error('name')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                
+                                        <div class="form-group row">
+                                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                
+                                            <div class="col-md-6">
+                                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                
+                                                @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                
+                                        <div class="form-group row">
+                                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                
+                                            <div class="col-md-6">
+                                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                
+                                                @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                
+                                        <div class="form-group row">
+                                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                
+                                            <div class="col-md-6">
+                                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                            </div>
+                                        </div>
+                
+                                        <div class="form-group row mb-0">
+                                            <div class="col-md-6 offset-md-4">
+                                                <button type="submit" class="btn btn-primary">
+                                                    {{ __('Register') }}
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <div class="floatingButtonWrap">
     <div class="floatingButtonInner">
         <a href="#" class="floatingButton">
@@ -341,7 +429,7 @@
                 <a href="#">Your Media</a>
             </li>
             <li>
-                <a href="#">Edit Profile</a>
+                <a href="" data-toggle="modal" data-target="#editprofileModal">Edit Profile</a>
             </li>
         </ul>
     </div>
